067c0762130b766f4d1a1ae2ecd1c8a3df1995ba
Mr Sharafdin <isasharafdin@gmail.com>
2025-03-15 03:31:33 +0300

    📌 Updated AST to support static typing with var_type property

---
+++ src/ast.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class NodeType(Enum):
+    PROGRAM = "PROGRAM"
+    VARIABLE_DECLARATION = "VARIABLE_DECLARATION"
+    FUNCTION_DEFINITION = "FUNCTION_DEFINITION"
+    FUNCTION_CALL = "FUNCTION_CALL"
+    IF_STATEMENT = "IF_STATEMENT"
+    LOOP_STATEMENT = "LOOP_STATEMENT"
+    BLOCK = "BLOCK"
+    BINARY_OPERATION = "BINARY_OPERATION"
+    LITERAL = "LITERAL"
+    IDENTIFIER = "IDENTIFIER"
+    CLASS_DEFINITION = "CLASS_DEFINITION"
+    IMPORT_STATEMENT = "IMPORT_STATEMENT"
+    TRY_CATCH = "TRY_CATCH"
+    BREAK_STATEMENT = "BREAK_STATEMENT"
+    CONTINUE_STATEMENT = "CONTINUE_STATEMENT"
+
+
+class ASTNode:
+    def __init__(self, type_, value=None, children=None):
+        self.type = type_
+        self.value = value
+        self.children = children if children else []
+        self.var_type = None  # For static typing
+
+    def __repr__(self):
+        type_info = ""
+        if self.type == NodeType.VARIABLE_DECLARATION and hasattr(self, 'var_type') and self.var_type is not None:
+            type_info = f", var_type={self.var_type}"
+        return f"ASTNode({self.type}, value={self.value}{type_info}, children={self.children})"
