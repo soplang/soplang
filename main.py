afe20c9dbd91c92a857376c0726d0259b134ed65
Mr Sharafdin <isasharafdin@gmail.com>
2025-03-15 03:29:10 +0300

    📌 Updated main.py with src imports and static typing examples

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from lexer import Lexer
-from parser import Parser
-from interpreter import Interpreter
+from src.lexer import Lexer
+from src.parser import Parser
+from src.interpreter import Interpreter
 
 code = r"""
+# Dynamic typing
 door x = 10
 qor(x + 5)
+
+# Static typing
+tiro y = 20
+qoraal naam = "Sharafdin"
+qor(naam + " " + qoraal(y))
 
 haddii (x > 5) {
     qor("X waa weyn!")
@@ -14,13 +20,15 @@ haddii (x > 5) {
     qor("X waa yar yahay!")
 }
 
+# Try-catch example
 isku_day {
-    door y = 5 / 0
+    door z = 5 / 0
     qor("Never see this!")
 } qabo (err) {
     qor("Khalad baa dhacay: " + err)
 }
 
+# Loop example
 ku_celi i min 1 ilaa 5 {
     qor("i="+ i)
     haddii (i == 3) {
@@ -28,11 +36,13 @@ ku_celi i min 1 ilaa 5 {
     }
 }
 
+# Function example
 howl showDouble(num) {
     qor(num * 2)
 }
 showDouble(12)
 
+# Class examples
 fasalka Xayawaan {
     # In a real language, we'd define methods or fields
     qor("Xayawaan fasal")
@@ -46,6 +56,7 @@ ka_keen "another.sp"
 """
 
 # 1) Tokenize
+print("\n🔹 Running Soplang...")
 lexer = Lexer(code)
 tokens = lexer.tokenize()
 print("\n🔹 Tokens Generated:")
