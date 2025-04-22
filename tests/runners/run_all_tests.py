22b1bea8c5075c672c61df7e0bcde9333564f702
Omar-Tood <omarjibrilabdulkhadir@gmail.com>
2025-04-22 09:23:58 +0300

    Restructure test runners: Move to tests/runners directory

--- tests/runners/run_all_tests.py
+++ tests/runners/run_all_tests.py
@@ -3,8 +3,8 @@ import sys
 import os
 import unittest
 
-# Add the current directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# Add the project root directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 # Import test modules
 from tests.test_lexer import TestLexer
--- tests/runners/run_lexer_test.py
+++ tests/runners/run_lexer_test.py
@@ -2,8 +2,8 @@
 import sys
 import os
 
-# Add the current directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# Add the project root directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 # Import the test module
 from tests.test_lexer import TestLexer
